8496c947c3fc6db1d066c4b592eec1d2600f16c1
VictorConrado <estudantecomv@gmail.com>
2025-08-20 21:59:27 -0300

    Primeiro commit do projeto Capify

---
+++ Backend/src/models/album.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const albumSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true,
+        },
+    artist:{
+        type: String,
+        required: true,
+        },
+    imageUrl:{
+        type: String,
+        required: true,
+        },
+    songs:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Song"
+        }],
+},
+ { timestamps: true } // createdAtt, updatedAtt
+);
+
+export const Album = mongoose.model("Album", albumSchema);
---
+++ Backend/src/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import { connectDB} from './lib/db.js'; 
+
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import adminRoutes from './routes/admin.route.js';
+import songRoutes from './routes/songs.route.js';
+import albumRoutes from './routes/albums.route.js';
+import statRoutes from './routes/stats.route.js';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json()); //to parse req.body
+
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/admin", adminRoutes) 
+app.use("/api/songs", songRoutes) 
+app.use("/api/albums", albumRoutes)
+app.use("/api/stats", statRoutes)
+
+app.listen(PORT, () => {    
+ console.log('Server is running on port. ' + PORT);   
+ connectDB();
+})
